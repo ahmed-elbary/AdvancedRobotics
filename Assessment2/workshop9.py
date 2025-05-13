becbb85aa3e876729206f2c7433c1e177ebb2d11
ahmed-elbary <96688514+ahmed-elbary@users.noreply.github.com>
2025-05-13 20:51:03 +0100

    updates

--- Assessment2/workshop9.py
+++ Assessment2/workshop9.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import mean_squared_error # type: ignore
+from sklearn.metrics import mean_squared_error 
 import pydmps
 import pydmps.dmp_discrete
 from pydmps.dmp_discrete import DMPs_discrete
@@ -10,11 +10,11 @@ from pydmps.dmp_discrete import DMPs_discrete
 #check the explaination provided in internet
 ############ Load a demonstration trajectory as numpy array from the demos folder  ############
 # Load the trajectory from CSV into a NumPy array
-y_demo = np.loadtxt("C:/Users/Student/Desktop/AdvancedRobotics/Assessment2/datasets/Cshape.csv", delimiter=",")
+y_demo = np.loadtxt("D:/University of Lincoln/Semester B/Advanced Robotics/AdvancedRobotics/Assessment2/datasets/Line.csv", delimiter=",")
 
 # Print the shape and a few values to verify
-print("Trajectory shape:", y_demo.shape)
-print("First few points:\n", y_demo[:5])
+# print("Trajectory shape:", y_demo.shape)
+# print("First few points:\n", y_demo[:5])
 
 
 ############ Create the DMP object and set number of DMPs, RBFs and Starting state ############
@@ -22,7 +22,7 @@ print("First few points:\n", y_demo[:5])
 
 # Set parameters for the DMP
 n_dmps = 2        # Number of DMPs (2 for a 2D trajectory: x and y)
-n_bfs =  50         # Number of Radial Basis Functions
+n_bfs =  10         # Number of Radial Basis Functions
 start_state = y_demo[0]  # Starting state (first point of the demo trajectory)
 
 # Create the DMP object
